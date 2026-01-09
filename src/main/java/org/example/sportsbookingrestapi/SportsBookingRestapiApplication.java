c082d039263db8f37eaa9186adacd3d948b9f18d
Kartik Badiger <pattarsachin10@gmail.com>
2026-01-09 12:36:31 +0530

    first commit

---
+++ src/main/java/org/example/sportsbookingrestapi/SportsBookingRestapiApplication.java
@@ -0,0 +1,13 @@
+package org.example.sportsbookingrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsBookingRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsBookingRestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sportsbookingrestapi/SportsBookingRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sportsbookingrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsBookingRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
